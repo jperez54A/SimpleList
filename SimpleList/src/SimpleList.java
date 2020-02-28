35af8e9768cb3f026129e18918bedaff5a534226
Jesus <jperez54@asu.edu>
2020-02-27 18:54:46 -0700

    added add and remove functions that shrink or grow the array

--- SimpleList/src/SimpleList.java
+++ SimpleList/src/SimpleList.java
@@ -8,6 +8,77 @@ public class SimpleList {
 		count = 0 ;
 		list = new int[10];
 	}
-}
+	public void add(int numbToAdd) {
+		//make temp list so that we can transfer values correctly
+		int tempList[] = new int[list.length];
+
+		if (list.length == count) {
+
+			// for loop to populate the temp list 
+			for (int i = 0; i<= list.length; i++) {
+				tempList[i]= list[i];
+			}
+			//resizing by 50%
+			int tempsize = list.length + ((list.length)/2);
+			list = new int[tempsize];
+			
+			//filling up the array
+			//first we must put the number entered in the first element of the array
+			list[0] = numbToAdd;
+			
+			for(int i = 1; i<= count; i++) {
+				list[i] = tempList[i-1];
+			}
+		}
+		else if(list.length < count ) {
+			// for loop to populate the temp list 
+			for (int i = 0; i<= list.length; i++) {
+				tempList[i]= list[i];
+			}
+			
+			//filling up the array
+			//first we must put the number entered in the first element of the array
+			list[0] = numbToAdd;
+			
+			for(int i = 1; i<= count; i++) {
+				list[i] = tempList[i-1];
+			}
+		}
+	}
+		
+		public void remove(int numbToRemove) {
+			//for loop to traverse and check to see if the element is in the array
+			for(int i = 0; i<= count; i++) {
+				//if found remove depending if it is more than 25% full
+				if(list[i] == numbToRemove) {
+				
+				}
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
 	
 
---
+++ SimpleList/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleList list1 = new SimpleList();
+		list1.add(12);
+		list1.add(13);
+		list1.add(14);
+		list1.add(15);
+		list1.add(16);
+		list1.add(17);
+		
+	}
+
+}
